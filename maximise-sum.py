bfd4aa051912f2f73715e21b0c6aa9d7e602c146
devin shields <devin.shields@gmail.com>
2015-11-29 16:30:32 -0500

    runs but isn't correct

--- maximise-sum.py
+++ maximise-sum.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 '''
-
 PROBLEM:
     https://www.hackerrank.com/challenges/maximise-sum
         First line contains T, number of test cases to follow.
@@ -18,40 +17,14 @@ PROBLEM:
 
 NOTES:
     http://stackoverflow.com/questions/32406515/using-binary-search-to-find-the-sub-array-with-the-largest-sum-modulo-x-my-solu
-
+    
     https://en.wikipedia.org/wiki/Maximum_subarray_problem
     http://cplusplus-coding.blogspot.com/2015/07/maximise-sum.html
     http://stackoverflow.com/questions/31113993/maximum-subarray-sum-modulo-m
-
-More Notes:
-
-    # note: the solution indices (i, j) are two dimensional,
-    # but the output value is one dimensional (the max subset, modulo M).
-    # The dynamic programming insight here is that:
-    #
-    #   max_modulo_subset(index <= i) = max(max_modulo_subset(index<i), max_modulo_subset(index=i))
-    #
-    # This way, you can tack on numbers to the input array and the problem formulation still works.
-    # The tricks are:
-    #
-    #   1) loop over the input array and create a cumulative sum array, modulo M.
-    #   2) as you go, maintain a binary search tree of all values already seen
-    #       x. use this tree to search for the maximum mod-sum ending at element i (that is, search backwards).
-    #   3) track the max mod sum across all i
-    #
-    # Something bugs me about this formulation; it depends on a mutable data structure
-    # that updates as we progress through the list. The functional programmer inside
-    # me cringes. The example code I've seen is unreadable. I'll think about reformulating...
-    #
-
 '''
 
 
-#
-# Binary search tree:
-#
-#   https://docs.python.org/2/library/bisect.html#searching-sorted-lists
-#
+import bisect
 
 
 def get_cumsum(int_array, m):
@@ -63,14 +36,24 @@ def get_cumsum(int_array, m):
     return cumsum_arr
 
 
-def get_binary_search_tree(cumsum_arr):
-    # TODO
+class BinaryTree(object):
+    '''
+    https://docs.python.org/2/library/bisect.html#searching-sorted-lists
+    '''
+    def __init__(self, m, cumsum_arr):
+        self.m = m
+        self.cumsum_sorted_arr = sorted(cumsum_arr)
 
-    prev_sum := find_next(T, S[i])
-    if prev_sum is found:
-        max_ending_at_i := (S[i] - prev_sum) % m
-    max_mod_sum = max(max_mod_sum, max_ending_at_i)
-    return TODO
+    def lower_bound(self, partial_sum):
+        ''' Find rightmost value less than or equal to partial_sum '''
+        i = bisect.bisect_right(self.cumsum_sorted_arr, partial_sum)
+        if i:
+            return self.cumsum_sorted_arr[i-1]
+        else:
+            return 0
+
+    def get_best_span_sum_for_partial_sum(self, partial_sum):
+        return (partial_sum - self.lower_bound(partial_sum)) % self.m
 
 
 def main():
@@ -81,28 +64,20 @@ def main():
         int_array = map(int,raw_input().strip().split(' '))
 
         # create the cumsum array
-        cumsum_arr = get_cumsum(int_array)
+        cumsum_arr = get_cumsum(int_array, m)
 
         # create a binary tree for O(log(n)) searching
-        binary_tree = get_binary_search_tree(cumsum_arr)
+        binary_tree = BinaryTree(m, cumsum_arr)
     
-        # create temp variable for the final answer, any valid candidate solution will do.
+        # create a var for the final answer
         maxsum = int_array[0] % m
 
-        # for each element in the cumsum array, search for the corresponding
-        # element that maximizes:
-        #         sum(int_array[i:j]) % m
         for partial_sum in cumsum_arr:
-            # search for the best matching partial sum
-            binary_tree.search()
-
-            #
-            prev_sum = find_next(T, S[i])
-            if prev_sum is found:
-                max_ending_at_i = (S[i] - prev_sum) % m
-            max_mod_sum = max(max_mod_sum, max_ending_at_i)
+            # search for the best matching partial sum, the smallest number bigger than the partial_sum
+            max_at_i = binary_tree.get_best_span_sum_for_partial_sum(partial_sum)
+            maxsum = max(maxsum, max_at_i)
         
-        print
+        print maxsum
 
 
 if __name__ == '__main__':
